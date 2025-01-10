86a36698fda9edb61b9b652af425687055c404df
Phillipa Cooper <phillipacooper0@gmail.com>
2025-01-10 14:38:45 +1100

    adding duck gif and wriggling with a bit of css

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import styles from './Header.module.css'
+import { motion } from "framer-motion";
+import video1 from "./ripple.gif"
 
 const CustomHeader = () => {
     return (
-    <div>
-        <h2 className={styles.header}>Phillipa Cooper</h2>
+    <motion.header
+    initial={{ opacity: 0, y: -50 }}
+    animate={{ opacity: 1, y: 0 }}
+    transition={{ duration: 0.5 }}
+      >  
+    <div className={styles.header}>
+        <div class="tenor-gif-embed" data-postid="24800350" data-share-method="host" data-aspect-ratio="1" data-width="5%"><a href="https://tenor.com/view/sei-la-gif-24800350">Sei La GIF</a>from <a href="https://tenor.com/search/sei+la-gifs">Sei La GIFs</a></div> <script type="text/javascript" async src="https://tenor.com/embed.js"></script>        
+        <h1>Phillipa Cooper</h1>
     </div>
+    </motion.header>
     )
 }
 export default CustomHeader;
