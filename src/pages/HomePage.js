54d964cd9efe4937c7510d5cfa8d48460a66b445
Phillipa Cooper <phillipacooper0@gmail.com>
2025-01-24 15:07:03 +1100

    added more content for landing page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,13 +1,34 @@
 import React from 'react';
 import homePage from '../content/homepage.json'
+import profilePicture from '../content/profilepicture.jpg'
 import styles from './background.module.css'
 
 
 const HomePage = () => {
     return (
         <div className={styles.standard}>
-            <p>This is some homepage text</p>
-            <p>{homePage.name}</p>    
+            <div className={styles.container}>
+                <img src={profilePicture}>
+                </img>
+                <div>
+                    <div className={styles.container}>
+                        <h8>Name:</h8>
+                        <p>{homePage.name}</p>
+                    </div>
+                    <div className={styles.container}>
+                        <h8>Current Role:</h8>
+                        <p>{homePage.currentRole}</p>
+                    </div>
+                    <div className={styles.container}>
+                        <h8 >Current Workplace:</h8>
+                        <p>{homePage.currentWorkplace}</p>
+                    </div>
+                    <div className={styles.container}>
+                        <h8 >Currently Upskilling:</h8>
+                        <p>{homePage.currentUpskilling}</p>
+                    </div>      
+                </div>
+            </div>
         </div>
     )
 }
