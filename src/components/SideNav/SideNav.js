22f2bcde6ff9941907b683bf2594a865d495738c
Phillipa Cooper <phillipacooper0@gmail.com>
2025-02-20 12:40:33 +1100

    fixed home page. hopefully fixed sidenav routing

--- src/components/SideNav/SideNav.js
+++ src/components/SideNav/SideNav.js
@@ -5,11 +5,11 @@ const SideNav = ({isClicked}) => {
     return (
         <nav className={`${styles.navbar} ${isClicked ? styles.open : ''}`}>            
             <ul>
-                <li><a href="/">Home</a></li>
-                <li><a href="/about">About</a></li>
-                <li><a href="/workexperience">Work Experience</a></li>
-                <li><a href="/projects">Projects</a></li>
-                <li><a href="/contacts">Contacts</a></li>
+                <li><a href="/phillipacooper/">Home</a></li>
+                <li><a href="/phillipacooper/#/about">About</a></li>
+                <li><a href="/phillipacooper/#/workexperience">Work Experience</a></li>
+                <li><a href="/phillipacooper/#/projects">Projects</a></li>
+                <li><a href="/phillipacooper/#/contacts">Contacts</a></li>
             </ul>
         </nav>
     )
