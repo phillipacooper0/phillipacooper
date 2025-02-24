996fdfd26ae11acaa773ba4a3aa6ee20edfc092e
Phillipa Cooper <phillipacooper0@gmail.com>
2025-02-24 11:17:56 +1100

    fixed css for mobile view

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -11,11 +11,11 @@ const NavBar = () => {
 
     return (
         <div className={styles.navbar} title="navbar" >
-            <Link to="/phillipacooper/#/" onClick={(e) => handleClick('home', e)} className={`${styles.navbarLink} ${activeLink === 'home' ? styles.navbarLinkActive : ''}`}>Home Page</Link>
-            <Link to="/phillipacooper/#/about" onClick={(e) => handleClick('about', e)} className={`${styles.navbarLink} ${activeLink === 'about' ? styles.navbarLinkActive : ''}`}>About Me</Link>
-            <Link to="/phillipacooper/#/workexperience" onClick={(e) => handleClick('workexperience', e)} className={`${styles.navbarLink} ${activeLink === 'workexperience' ? styles.navbarLinkActive : ''}`}>Work Experience</Link>
-            <Link to="/phillipacooper/#/projects" onClick={(e) => handleClick('projects', e)} className={`${styles.navbarLink} ${activeLink === 'projects' ? styles.navbarLinkActive : ''}`}>Projects</Link>
-            <Link to="/phillipacooper/#/contacts" onClick={(e) => handleClick('contacts', e)} className={`${styles.navbarLink} ${activeLink === 'contacts' ? styles.navbarLinkActive : ''}`}>Contacts</Link>
+            <Link to="/#/" onClick={(e) => handleClick('home', e)} className={`${styles.navbarLink} ${activeLink === 'home' ? styles.navbarLinkActive : ''}`}>Home Page</Link>
+            <Link to="/#/about" onClick={(e) => handleClick('about', e)} className={`${styles.navbarLink} ${activeLink === 'about' ? styles.navbarLinkActive : ''}`}>About Me</Link>
+            <Link to="/#/workexperience" onClick={(e) => handleClick('workexperience', e)} className={`${styles.navbarLink} ${activeLink === 'workexperience' ? styles.navbarLinkActive : ''}`}>Work Experience</Link>
+            <Link to="/#/projects" onClick={(e) => handleClick('projects', e)} className={`${styles.navbarLink} ${activeLink === 'projects' ? styles.navbarLinkActive : ''}`}>Projects</Link>
+            <Link to="/#/contacts" onClick={(e) => handleClick('contacts', e)} className={`${styles.navbarLink} ${activeLink === 'contacts' ? styles.navbarLinkActive : ''}`}>Contacts</Link>
         </div>
     )
 }
