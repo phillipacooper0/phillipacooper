6d8d5bf24532f74b1a5fe8758881b31b48d17100
Phillipa Cooper <phillipacooper0@gmail.com>
2025-02-25 09:19:58 +1100

    added tests for webdisplay

--- src/components/Header/WebDisplay.test.js
+++ src/components/Header/WebDisplay.test.js
@@ -10,6 +10,18 @@ test("WebDisplay renders successfully", () => {
     expect(element).toBeInTheDocument();
 })
 
+test("Header contains href to /phillipacooper", () => {
+    render(<Router><Display/></Router>)
+    const homePage = screen.getByText(/Phillipa Cooper/)
+    expect(homePage).toHaveAttribute('href', '/phillipacooper')
+})
+
+test("Duck walking gif renders successfully", () => {
+    render(<Router><Display/></Router>)
+    const element = screen.getByAltText(/walking duck/i);
+    expect(element).toBeInTheDocument();
+})
+
 test("WebDisplay contains img for linkedIn", () => {
     render(<Router><Display/></Router>)
     const image = screen.getByAltText(/linkedIn logo/)
@@ -20,4 +32,16 @@ test("WebDisplay contains img for github", () => {
     render(<Router><Display/></Router>)
     const image = screen.getByAltText(/github logo/i)
     expect(image).toBeInTheDocument()
+})
+
+test("WebDisplay contains NavBar component", () => {
+    render(<Router><Display/></Router>)
+    const homePage = screen.getByText(/Home Page/)
+    expect(homePage).toBeTruthy();
+})
+
+test("Header contains Phillipa Cooper as text", () => {
+    render(<Router><Display/></Router>)
+    const homePage = screen.getByText(/Phillipa Cooper/)
+    expect(homePage).toBeTruthy();
 })
